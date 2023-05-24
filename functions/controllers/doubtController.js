f0d48b933d2d577ddae2fd9d66340a86fff44884
vaibhav gupta <vaibhavg4651@gmail.com>
2023-05-24 19:55:46 +0530

    made post api

---
+++ functions/controllers/doubtController.js
@@ -0,0 +1,21 @@
+
+const { getFirestore  } = require("firebase-admin/firestore");
+const db = getFirestore()
+
+
+
+const addDoubt = async(req, res, next)=>{
+    const {description,downVotes,heading, score,upVotes,userName} = req.body
+    try {
+        await db.collection('AllDoubts').doc().create({
+            description,downVotes,heading, score,upVotes,userName
+        });
+        res.status(200).send("doubt saved successfully");
+    } catch (error) {
+        res.status(400).send(error.message);
+        console.log(req.body);
+    }
+}
+
+
+module.exports ={ addDoubt };
