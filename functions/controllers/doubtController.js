2e9b42c69871fc34d25a8d70b802e588d7ea0208
vaibhav gupta <vaibhavg4651@gmail.com>
2023-05-27 15:37:16 +0530

    connect to redis

--- functions/controllers/doubtController.js
+++ functions/controllers/doubtController.js
@@ -7,7 +7,7 @@ const db = getFirestore()
 const addDoubt = async(req, res, next)=>{
     const {description,downVotes,heading, score,upVotes,userName} = req.body
     try {
-        await db.collection('AllDoubts').doc().create({
+        await db.collection('doubts').doc().create({
             description,downVotes,heading, score,upVotes,userName
         });
         res.status(200).send("doubt saved successfully");
@@ -15,6 +15,13 @@ const addDoubt = async(req, res, next)=>{
         res.status(400).send(error.message);
         console.log(req.body);
     }
+
+
+
+
+
+
+    
 }
 
 
--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,29 @@
 const functions = require("firebase-functions");
+const dotenv = require("dotenv")
 const { onRequest } = require("firebase-functions/v2/https");
 const admin = require("firebase-admin");
+const redis = require("redis");
 var serviceAccount = require("./doubtless-bd798-firebase-adminsdk-5sxx2-18b56b565c.json");  //add the path of the admin SDK credentials file
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://doubtless-bd798.firebaseio.com'   
 });
+
+
+ dotenv.config();
+
+const {HOST , PORT , PASSWORD} = process.env;
+const redisClient = redis.createClient({
+  password: PASSWORD,
+  socket: {
+      host: HOST,
+      port: PORT
+  }
+});
+
+await client.connect();
+
 
 const doubtRoutes = require("./routes/doubt-route");
 const postAnswer = require("./routes/post_answer");
