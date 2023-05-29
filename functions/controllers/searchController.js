4174d2172d05adbef6a58f1af23145a8527132de
himanshuaggar <himanshuaggar00@gmail.com>
2023-05-29 17:09:32 +0530

    changes in search api

--- functions/controllers/searchController.js
+++ functions/controllers/searchController.js
@@ -1,41 +1,47 @@
 const Redis = require('ioredis');
-const redisClient = new Redis();
+const redisClient = new Redis({
+  port: , 
+  host: "", 
+  username: "",
+  password: "",
+  
+});
 
 const searchAnswer = async(req, res) => {
-  const queryArray = req.query.queryArray;
+  let queryArray = req.query.queryArray;
+
+  // Split the query string into an array of words and convert to lowercase
+  queryArray = queryArray.toString().toLowerCase().split(' ');
+  queryArray = queryArray.toString().split(',').map(query => query.trim());
+  console.log(queryArray)
+  
 
   try {
-    const keys = await redisClient.keys('*');
-    const matchingKeys = keys.filter(key => {
-      const keyWords = key.split(' ');
-      return queryArray.some(word => keyWords.includes(word.toLowerCase()));
-    });
-
     const matchingData = [];
-    const pipeline = redisClient.pipeline();
 
-    matchingKeys.forEach(key => {
-      pipeline.get(key);
-    });
+    const keys = await redis.keys('*');
+    const values = await redis.mget(...keys);
 
-    const results = await pipeline.exec();
+    for (const value of values) {
+      const data = JSON.parse(value);
 
-    results.forEach(result => {
-      const value = result[1];
-      if (value !== null) {
-        const data = JSON.parse(value);
+      const stringValue = typeof data === 'string' ? data : JSON.stringify(data);
+
+      if (queryArray.some(query => stringValue.toLowerCase().includes(query.toLowerCase()))) {
         matchingData.push(data);
       }
-    });
+    }
 
-    res.status(200).json(matchingData);
+    // Return the result if any matching data is found
+    if (matchingData.length > 0) {
+      res.status(200).json(matchingData);
+    } else {
+      res.status(404).json({ message: 'No matching data found' });
+    }
   } catch (error) {
     console.error('Error searching for data in Redis:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
   }
-
-
-  
 
 module.exports = {searchAnswer }
