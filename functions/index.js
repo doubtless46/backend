b19572658154d68f898514829c1e1dbe3c607973
himanshuaggar <himanshuaggar00@gmail.com>
2023-06-20 22:12:56 +0530

    env changes

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,22 @@ const functions = require("firebase-functions");
 const { onRequest } = require("firebase-functions/v2/https");
 const admin = require("firebase-admin");
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const serviceAccount =  require("add the path of the admin SDK credentials file");
+// const serviceAccount =  require("add the path of the admin SDK credentials file");
+
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://projectname.firebaseio.com' 
+  // credential: admin.credential.cert(serviceAccount),
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASURMENT_ID,
+  databaseURL: process.env.FIREBASE_DB_URL 
 });
 
 
