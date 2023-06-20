5f289779e8aa0bedb9ab7f971079dc3c3fa497ef
Kartikay Asija <kartikay7057@gmail.com>
2023-06-20 17:17:20 +0530

    env

--- functions/controllers/searchController.js
+++ functions/controllers/searchController.js
@@ -1,5 +1,5 @@
 const Redis = require('ioredis');
-const client = new Redis({
+const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
   password: process.env.REDIS_PASSWORD,
