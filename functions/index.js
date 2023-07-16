25258c9555b6918d46112ad598410945d80c9922
Rolika Agarwal <rolikaagarwal2003@gmail.com>
2023-07-17 04:23:32 +0530

    .

--- functions/index.js
+++ functions/index.js
@@ -3,19 +3,24 @@ const functions = require("firebase-functions");
 const { onRequest } = require("firebase-functions/v2/https");
 const admin = require("firebase-admin");
 const bodyParser = require("body-parser");
-
+const serviceAccount =  require("./adminSDK.json");
 
 admin.initializeApp({
-  // credential: admin.credential.cert(serviceAccount),
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASURMENT_ID,
-  databaseURL: process.env.FIREBASE_DB_URL 
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.DATABASE_URL,
 });
+
+// admin.initializeApp({
+//   // credential: admin.credential.cert(serviceAccount),
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_,
+//   appId: process.env.FIREBASE_APP_ID,
+//   measurementId: process.env.FIREBASE_MEASURMENT_ID,
+//   databaseURL: process.env.FIREBASE_DB_URL 
+// });
 
 const doubtRoutes = require("./routes/doubt-route");
 const postAnswer = require("./routes/post_answer");
